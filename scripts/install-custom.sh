f81376d0e33bdef88bccbc868d3504507d94c701
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-02-04 14:46:32 +0100

    fix: provisioning script

--- scripts/install-custom.sh
+++ scripts/install-custom.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
-
+set -e
 # make sure we can get the apt lock before running the plays
-until sudo apt-get update; do echo "Waiting for apt-get lock"; sleep 5; done
+until ping -c 1 8.8.8.8; do echo "Internet down...sleeping for 5..."; sleep 5; done
+until sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y traceroute squid net-tools
+  do echo "Waiting for apt-get lock"
+    sleep 5
+  done
 
 ## install apps
 sudo snap install eclipse --classic
 sudo snap install intellij-idea-community --classic
 sudo snap install goland --classic
-sudo apt install traceroute squid net-tools
 
 ## install java, gradle and maven
-sdk i java
-$JAVA_8=$(sdk list java|grep '8.0'|grep open|head -1|cut -d"|" -f6)
-sdk i java $JAVA_8 < /dev/null
-sdk i gradle < /dev/null
-sdk i maven < /dev/null
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+yes | sdk install java
+# install JAVA 8
+yes | sdk install java "$(sdk list java | grep 8.0 | grep zulu | head -1 | cut -d"|" -f6 | xargs)"
+yes | sdk install gradle
+yes | sdk install  maven
 
 ## get repositories
 git clone https://github.com/snyk/snyk-intellij-plugin
@@ -26,4 +31,5 @@ sudo systemctl enable squid
 sudo systemctl restart squid
 sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
 sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
-sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 80,443 -j REJECT
+sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 80 -j REJECT
+sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 443 -j REJECT
