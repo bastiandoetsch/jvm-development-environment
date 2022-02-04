64e7ab66324cfe67bcfbd22ccecc0d274625a1d2
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-02-04 14:49:34 +0100

    fix: make provisioning more error tolerant & rerunnable

--- scripts/install-custom.sh
+++ scripts/install-custom.sh
@@ -22,13 +22,14 @@ yes | sdk install gradle
 yes | sdk install  maven
 
 ## get repositories
-git clone https://github.com/snyk/snyk-intellij-plugin
-git clone https://github.com/snyk/snyk-eclipse-plugin
-git clone https://github.com/snyk/vscode-extension
+[[ ! -d "snyk-intellij-plugin" ]] && git clone https://github.com/snyk/snyk-intellij-plugin
+[[ ! -d "snyk-intellij-plugin" ]] && git clone https://github.com/snyk/snyk-eclipse-plugin
+[[ ! -d "snyk-intellij-plugin" ]] && git clone https://github.com/snyk/vscode-extension
 
 ## configure firewall to only work through proxy
 sudo systemctl enable squid
 sudo systemctl restart squid
+sudo iptables -F
 sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
 sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
 sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 80 -j REJECT
