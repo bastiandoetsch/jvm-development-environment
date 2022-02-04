5644abc8a3131ce664489cb535b4eecebd2b3c30
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-02-04 12:16:18 +0100

    block traffic to snyk.io that is not coming via proxy

--- scripts/install-custom.sh
+++ scripts/install-custom.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "" ]
-then
- echo "No user-specific Ansible playbook defined. Nothing to do."
- exit
-fi
-
 # make sure we can get the apt lock before running the plays
 until sudo apt-get update; do echo "Waiting for apt-get lock"; sleep 5; done
 
-# the GitHub location of the custom Ansible plays to run eg, kurron/ansible-pull-desktop-tweaks.git
-PROJECT=$1
+## install apps
+sudo snap install eclipse --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install goland --classic
+sudo apt install traceroute squid net-tools
 
-ansible-pull --checkout master \
-             --directory /tmp/ansible-pull-custom \
-             --inventory=localhost, \
-             --module-name=git \
-             --url=https://github.com/${PROJECT} \
-             --verbose \
-             --only-if-changed \
-             --user vagrant \
-             playbook.yml
+## install java, gradle and maven
+sdk i java
+$JAVA_8=$(sdk list java|grep '8.0'|grep open|head -1|cut -d"|" -f6)
+sdk i java $JAVA_8 < /dev/null
+sdk i gradle < /dev/null
+sdk i maven < /dev/null
 
+## get repositories
+git clone https://github.com/snyk/snyk-intellij-plugin
+git clone https://github.com/snyk/snyk-eclipse-plugin
+git clone https://github.com/snyk/vscode-extension
+
+## configure firewall to only work through proxy
+sudo systemctl enable squid
+sudo systemctl restart squid
+sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
+sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 80,443 -j REJECT
