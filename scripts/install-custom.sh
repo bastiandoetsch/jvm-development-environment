0ef45875d9e281cd6d9910958e36afe2c0325bdf
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-02-05 09:57:06 +0100

    feat: ✨ set timezone to CET and don't only reject snyk.io but all URLs without proxy

--- scripts/install-custom.sh
+++ scripts/install-custom.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 # make sure we can get the apt lock before running the plays
+sudo timedatectl set-timezone "Europe/Berlin"
 until ping -c 1 8.8.8.8; do echo "Internet down...sleeping for 5..."; sleep 5; done
 until sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y traceroute squid net-tools
   do echo "Waiting for apt-get lock"
@@ -11,6 +12,8 @@ until sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y 
 sudo snap install eclipse --classic
 sudo snap install intellij-idea-community --classic
 sudo snap install goland --classic
+
+##
 
 ## install java, gradle and maven
 export SDKMAN_DIR="$HOME/.sdkman"
@@ -32,5 +35,5 @@ sudo systemctl restart squid
 sudo iptables -F
 sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
 sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
-sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 80 -j REJECT
-sudo iptables -A OUTPUT -p tcp -d snyk.io --dport 443 -j REJECT
+sudo iptables -A OUTPUT -p tcp --dport 80 -j REJECT
+sudo iptables -A OUTPUT -p tcp --dport 443 -j REJECT
