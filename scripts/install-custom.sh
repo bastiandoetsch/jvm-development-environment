a8822e56f015ab142405f5d040c277b00d68b10f
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-01-04 09:16:35 +0100

    feat: use sslsplit to use mitm certificates

--- scripts/install-custom.sh
+++ scripts/install-custom.sh
@@ -2,13 +2,26 @@
 set -e
 sudo timedatectl set-timezone "Europe/Berlin"
 sudo iptables -F
-until ping -c 1 8.8.8.8; do echo "Internet down...sleeping for 5..."; sleep 5; done
+until ping -c 1 8.8.8.8; do
+  echo "Internet down...sleeping for 5..."
+  sleep 5
+done
+export DEBIAN_FRONTEND=noninteractive
 
-until sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo apt-get install -y traceroute squid net-tools unzip zip tasksel ubuntu-gnome-desktop git python3 iptables-persistent
-  do
-    echo "Waiting for apt-get lock"
-    sleep 5
-  done
+#until sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo apt-get install -y traceroute squid net-tools unzip zip tasksel ubuntu-gnome-desktop git python3 iptables-persistent
+until sudo apt-get update &&
+  sudo apt-get install -y traceroute \
+    sslsplit \
+    net-tools \
+    unzip zip \
+    tasksel \
+    ubuntu-gnome-desktop \
+    git \
+    python3 ; do
+  echo "Waiting for apt-get lock"
+  sleep 5
+done
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo apt-get autoremove -y && sudo apt-get clean -y
 
 ## install apps
@@ -16,7 +29,6 @@ sudo snap install intellij-idea-ultimate --classic
 sudo snap install intellij-idea-community --classic
 sudo snap install code --classic
 sudo snap install code-insiders --classic
-sudo snap install sublime-text --classic
 sudo snap install sublime-text --classic
 sudo snap install node --classic
 sudo snap install go --classic
@@ -34,19 +46,30 @@ yes | sdk install maven
 
 ## get repositories
 [[ ! -d "snyk-intellij-plugin" ]] && git clone https://github.com/snyk/snyk-intellij-plugin
+[[ ! -d "snyk-intellij-plugin" ]] && git clone https://github.com/snyk/snyk-ls
 [[ ! -d "snyk-eclipse-plugin" ]] && git clone https://github.com/snyk/snyk-eclipse-plugin
 [[ ! -d "vscode-extension" ]] && git clone https://github.com/snyk/vscode-extension
+[[ ! -d "vscode-extension" ]] && git clone https://github.com/snyk/snyk-visual-studio-plugin
+[[ ! -d "vscode-extension" ]] && git clone https://github.com/snyk/snyk-eclipse-plugin
 
 ## configure firewall to only work through proxy
-sudo systemctl enable squid
-sudo systemctl restart squid
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+openssl genrsa -out ca.key 4096
+openssl req -new -x509 -days 1826 -key ca.key -out ca.crt -subj "/C=de/O=fake/OU=fakeorgunit/CN=fake.com"
+sudo mkdir -p /tmp/sslsplit
+sudo nohup sslsplit -l connections.log -j /tmp/sslsplit/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080 &
+
+
+sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner vagrant --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner vagrant --dport 443 -j REDIRECT --to-ports 8443
+
 sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
 sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 80 -j REJECT
 sudo iptables -A OUTPUT -p tcp --dport 443 -j REJECT
-sudo su -c 'mkdir -p /etc/iptables/'
-sudo su -c 'iptables-save > /etc/iptables/rules.v4'
+
 sudo systemctl set-default graphical.target
-reboot
+sudo systemctl isolate graphical.target
